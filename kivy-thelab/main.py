49dba65b870910a73f8fd134f89f178706731805
Your Name <mark.hong@outlook.com.au>
2021-05-26 22:45:30 +1000

    embeded layout learning

--- kivy-thelab/main.py
+++ kivy-thelab/main.py
@@ -1,8 +1,22 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
+class Environment(BoxLayout):
+    pass
+    """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "vertical"
+        b1 = Button(text = "Hello")
+        b2 = Button(text = "Bye")
+        self.add_widget(b1)
+        self.add_widget(b2)
+    """
 
 class MainWidget(Widget):
-    pass
+    pass 
 
 class TheLabApp(App):
     pass
