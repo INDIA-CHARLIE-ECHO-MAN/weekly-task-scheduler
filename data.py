670afff516c2aee1a0a143c726dd06d2292b4ba7
Your Name <mark.hong@outlook.com.au>
2021-05-28 23:44:15 +1000

    added data file for back end and adding doco on backend

---
+++ data.py
@@ -0,0 +1,31 @@
+data = {
+    "Open": [],
+    "Monday": [
+        "Open": [],
+        "Closed": []
+    ],
+    "Tuesday": [
+        "Open": [],
+        "Closed": []
+    ],
+    "Wednesday": [
+        "Open": [],
+        "Closed": []
+    ],
+    "Thursday": [        
+        "Open": [],
+        "Closed": []
+        ],
+    "Friday": [        
+        "Open": [],
+        "Closed": []
+        ],
+    "Saturday": [       
+        "Open": [],
+        "Closed": []
+        ],
+    "Sunday": [        
+        "Open": [],
+        "Closed": []
+        ]
+}
